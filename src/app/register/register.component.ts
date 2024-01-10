b3570c37dffff10a72af09856ee2d5f46de5ca40
DESKTOP-N0LK9DB\nemanja <nemanjamilosevic117@gmail.com>
2024-01-10 18:19:46 +0100

    Registracija dizajn zavrsen, validacija uradjena

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { confirmPasswordValidator } from '../validators/password.validator';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-
+  username = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(50),
+  ]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}'),
+  ]);
+  repeatPassword = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}'),
+    // Validators.minLength(8),
+  ]);
+  email = new FormControl('', [Validators.required, Validators.maxLength(255)]);
+  name = new FormControl('', [Validators.required, Validators.maxLength(255)]);
+  surname = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(255),
+  ]);
+  city = new FormControl('', [Validators.required, Validators.maxLength(255)]);
+  image = new FormControl(null);
+  constructor(private fb: FormBuilder) {}
+  profileForm = this.fb.group(
+    {
+      username: this.username,
+      password: this.password,
+      repeatPassword: this.repeatPassword,
+      email: this.email,
+      name: this.name,
+      surname: this.surname,
+      city: this.city,
+      image: this.image,
+    },
+    { validators: confirmPasswordValidator }
+  );
 }
