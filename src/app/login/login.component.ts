a56116eec3a8dea5684b12a03c572481f307d92e
DESKTOP-N0LK9DB\nemanja <nemanjamilosevic117@gmail.com>
2024-01-10 12:13:00 +0100

    login dizajn

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,14 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {AbstractControl, FormBuilder, Validators,FormGroup, FormControl,ReactiveFormsModule } from "@angular/forms";
-import {NgClass,NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,NgClass,NgIf ],
+  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  constructor(private formBuilder: FormBuilder) {
-  }
 
-
-  loginForm = this.formBuilder.group({
-    email: [undefined,[Validators.email, Validators.required]],
-    password: [null, [Validators.required]]
-  })
-
-  get controls(): { [p: string]: AbstractControl } {
-    return this.loginForm.controls;
-  }
 
 }
